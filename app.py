f7cbc491618fe654cbdbca03a1f47e91d4cb0270
laksamy6-dev <laksamy6@gmail.com>
2026-02-11 01:18:38 +0400

    Update app.py

--- app.py
+++ app.py
@@ -383,5 +383,5 @@ if st.session_state.bot_active:
     log_html = "".join([l for l in st.session_state.live_logs])
     log_ph.markdown(f'<div style="height:200px; overflow-y:auto; border:2px solid #000; padding:5px;">{log_html}</div>', unsafe_allow_html=True)
 
-    time.sleep(0.1)
+    time.sleep(0.0001)
     if not st.session_state.pending_signal: st.rerun()
